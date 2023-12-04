dd6c9608e8ffb9b4421b419cb02d556947e5f799
egoridze74 <evkorneenko@edu.hse.ru>
2023-12-04 21:11:53 +0700

    Very close to be good

--- Planet.cpp
+++ Planet.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "Planet.h"
+#include <cstring>
+#include <iostream>
+
+
+//Constructors, destructor
+Planet::Planet() //default constructor
+{
+    name = "";
+    x = y = z = mass = volume = 0.0;
+    livable = false;
+}
+
+Planet::Planet(const Planet &other) //copy constructor
+{
+    x = other.x;
+    y = other.y;
+    z = other.z;
+    mass = other.mass;
+    volume = other.volume;
+    name = new char[std::strlen(other.name)];
+    std::strcpy(name, other.name);
+}
+
+Planet::Planet(char *name, double x, double y, double z, double volume, double mass, bool livable) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+    this->mass = mass;
+    this->volume = volume;
+    this->name = new char[std::strlen(name)];
+    std::strcpy(this->name, name);
+}
+
+Planet::~Planet() //destructor
+{
+    if (mass != 0.0) delete[] name;
+    x = y = z = mass = volume = 0;
+    livable = false;
+
+}
+
+Planet& Planet::operator=(const Planet &other)
+{
+    if (this != &other)
+    {
+        this->name = new char[std::strlen(other.name)];
+        std::strcpy(this->name, other.name);
+        this->x = other.x;
+        this->y = other.y;
+        this->z = other.z;
+        this->mass = other.mass;
+        this->volume = other.volume;
+    }
+    return *this;
+}
+
+
+//Sorting
+void sort_by_density(Planet *planets, size_t n)
+{
+    for (size_t i = 0; i < n - 1; i++)
+        for (size_t j = 0; j < n - i - 1; j++) {
+            double dens1 = planets[j].get_density();
+            double dens2 = planets[j + 1].get_density();
+            if (dens1 > dens2) {
+                std::swap(planets[j], planets[j + 1]);
+            }
+        }
+}
--- Planet.h
+++ Planet.h
@@ -1,41 +1,94 @@
 //
 // Created by Egor on 03.12.2023.
 //
+#include <cstring>
+#include <cmath>
+#ifndef CPP_LAB_5_6_PLANET_H
+#define CPP_LAB_5_6_PLANET_H
 
-#ifndef Planet_h
-#define Planet_h
-#define MAX_LENGTH = 100
-
-
-class Planet
-{
+class Planet {
 private:
     char *name;
     double x, y, z;
-    double weight;
+    double mass;
     double volume;
     bool livable;
 public:
     //Constructors, destructor
     Planet(); //default constructor
 
-    Planet(const Planet&); //copy constructor
+    Planet(const Planet &); //copy constructor
 
     Planet(char *name, double x, double y, double z, double volume, double weight, bool livable);
 
     ~Planet(); //destructor
 
-    //Getters
-    inline char* get_name() const;
-    inline double get_x() const;
-    inline double get_y() const;
-    inline double get_z() const;
-    inline double get_mass() const;
-    inline double get_volume() const;
-    inline bool get_livable() const;
+    Planet& operator=(const Planet &other);
 
-    //
+    //Getters, setters
+    inline const char* get_name() const {
+        return name;
+    }
+
+    inline double get_x() const {
+        return x;
+    }
+
+    inline double get_y() const {
+        return y;
+    }
+
+    inline double get_z() const {
+        return z;
+    }
+
+    inline double get_mass() const {
+        return mass;
+    }
+
+    inline double get_volume() const {
+        return volume;
+    }
+
+    inline bool get_livable() const {
+        return livable;
+    }
+
+    double get_distance(Planet &other) const {
+        double dist = sqrt(pow(x - other.get_x(), 2) + pow(y - other.get_y(), 2) + pow(z - other.get_z(), 2));
+        return dist;
+    }
+
+    double get_density() const {
+        double dens = mass / volume;
+        return dens;
+    }
+
+    inline void set_name(char* name) {
+        this->name = new char[std::strlen(name)];
+        std::strcpy(this->name, name);
+    }
+    void set_x(double x) {
+        this->x = x;
+    }
+    void set_y(double y) {
+        this->y = y;
+    }
+    void set_z(double z) {
+        this->z = z;
+    }
+    void set_mass(double mass) {
+        this->mass = mass;
+    }
+    void set_volume(double volume) {
+        this->volume = volume;
+    }
+    void set_livable(bool livable) {
+        this->livable = livable;
+    }
 };
 
 
-#endif //Planet_h
+void sort_by_density(Planet *planets, size_t n);
+
+#endif //CPP_LAB_5_6_PLANET_H
